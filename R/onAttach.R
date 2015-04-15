b5babb650653c5902d36b5060ce929c8c008c74d
daniel-conn17 <djconn17@gmail.com>
2015-04-14 21:12:29 -0700

    simplify instructions for installing WGCNA

--- R/onAttach.R
+++ R/onAttach.R
@@ -2,9 +2,7 @@
   if (!requireNamespace("WGCNA", quietly = TRUE)) {
       packageStartupMessage("It is highly recommended that WGCNA be installed.")
       packageStartupMessage("To do this, enter the following commands into the console:")
-      packageStartupMessage("source(\"http://bioconductor.org/biocLite.R\")")
-      packageStartupMessage("biocLite(\"impute\")")
-      packageStartupMessage("biocLite(\"preprocessCore\")") 
+      packageStartupMessage("setRepositories(ind=1:2)")
       packageStartupMessage("install.packages(\"WGCNA\")")
       packageStartupMessage(" ")
       packageStartupMessage("If this fails see http://labs.genetics.ucla.edu/horvath/CoexpressionNetwork/Rpackages/WGCNA/ \n for further information.")
